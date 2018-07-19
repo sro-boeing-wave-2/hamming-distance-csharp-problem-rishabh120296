b8ffc7efee042217a41de5f5da7519b314752fd8
Rishabh Agrawal <rishabh120296@gmail.com>
2018-07-19 11:48:48 +0530

    Hamming distance calculated and appropriate exceptions thrown

--- Hamming/Hamming.cs
+++ Hamming/Hamming.cs
@@ -7,7 +7,19 @@ namespace Hamming
         public static int Distance(string original, string current)
         {
             int hammingDistance = 0;
-            
+            if (original == null)
+                throw new ArgumentNullException("original");
+            if (current == null)
+                throw new ArgumentNullException("current");
+            if (original.Length != current.Length)
+                throw new ArgumentException("Hamming Distance can only be calculated over strings of equal length");
+            for (int i=0; i<original.Length; i++)
+            {
+                if (original[i] != current[i])
+                {
+                    hammingDistance += 1;
+                }
+            }
             return hammingDistance;
         }
     }
